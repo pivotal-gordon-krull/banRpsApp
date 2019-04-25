aa1b0853481b6e9eaf38fb72a4392c02869204aa
Alberto Resco Perez <alperez@pivotal.io>
2019-04-25 17:00:48 +0530

    First test passed in ui

--- web/spec/webSpec.js
+++ web/spec/webSpec.js
@@ -1,6 +1,26 @@
 import * as ReactDOM from "react-dom";
 import * as React from "react";
 
+class RPSApp extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    submitHandler() {
+        this.setState({
+            result: 'INVALID!'
+        })
+    }
+
+    render() {
+        return <div>
+            {this.state.result}
+            <button onClick={this.submitHandler.bind(this)}>PLAY</button>
+        </div>
+    }
+}
+
 describe("play form", function () {
     describe("when the play use case tells the UI the input is invalid", function () {
         it('should tell the user that their input is invalid', function () {
@@ -15,6 +35,9 @@ describe("play form", function () {
                 <RPSApp requests={alwaysInvalidRequest}/>,
                 domFixture
             );
+
+
+            expect(domFixture.innerText).not.toContain('INVALID!');
 
             document.querySelector('button').click();
 
