e5a1e7225fa196f94cef51f58b71c6f537bdccec
Alberto Resco Perez <alperez@pivotal.io>
2019-04-25 16:21:42 +0530

    First web failing test

--- web/spec/webSpec.js
+++ web/spec/webSpec.js
@@ -1,5 +1,24 @@
-describe("foo", function () {
-    it("bars", function () {
-        expect(true).toBe(false)
+import * as ReactDOM from "react-dom";
+import * as React from "react";
+
+describe("play form", function () {
+    describe("when the play use case tells the UI the input is invalid", function () {
+        it('should tell the user that their input is invalid', function () {
+            let domFixture = document.createElement('div');
+            document.body.appendChild(domFixture);
+
+            let alwaysInvalidRequest = {
+                play: (p1, p2, observer) => observer.invalid()
+            }
+
+            ReactDOM.render(
+                <RPSApp requests={alwaysInvalidRequest}/>,
+                domFixture
+            );
+
+            document.querySelector('button').click();
+
+            expect(domFixture.innerText).toContain('INVALID!');
+        });
     })
 })
